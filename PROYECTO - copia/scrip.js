1c5075432614870dac1d0725a8b044bd1ae44502
MariaGuadalupe1822 <124002855+MariaGuadalupe1822@users.noreply.github.com>
2024-06-19 20:58:58 -0600

    Add files via upload

---
+++ PROYECTO - copia/scrip.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var app = document.getElementById('typewriter');
+  
+    var typewriter = new Typewriter(app, {
+      loop: true
+    });
+  
+    typewriter.typeString('Victor Hugo Bahena Guzman')
+      .pauseFor(2500)
+      .deleteAll()
+      .typeString('Un gran amigo y escritor')
+      .pauseFor(2500)
+      .start();
+  });
+  
+  
+  
